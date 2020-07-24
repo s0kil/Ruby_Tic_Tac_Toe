fc861fc201d09801410519fad6e7e4c5e4898efb
Daniel Sokil <dtechtours@gmail.com>
2020-07-24 11:21:20 -0400

    Fix Rubocop Errors

--- lib/Ruby_Tic_Tac_Toe.rb
+++
@@ -1,6 +0,0 @@
-require "Ruby_Tic_Tac_Toe/version"
-
-module RubyTicTacToe
-  class Error < StandardError; end
-  # Your code goes here...
-end
--- lib/Ruby_Tic_Tac_Toe/version.rb
+++
@@ -1,3 +0,0 @@
-module RubyTicTacToe
-  VERSION = "0.1.0"
-end

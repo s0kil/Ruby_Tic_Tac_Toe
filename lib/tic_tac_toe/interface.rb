05a507899fc5d8c70a2b26ca8a8c2e9b097ba2e1
s0kil <dtechtours@gmail.com>
2020-07-29 18:08:42 -0400

    Add available_game_board_columns To Restrict User Options Refactored player_prompt Into A Seperate Method, With ctrl+c Error Handling

--- lib/tic_tac_toe/interface.rb
+++ lib/tic_tac_toe/interface.rb
@@ -16,7 +16,7 @@ Player One will be 'X' and Player Two will be 'O')
       @game_matrix = game_matrix
     end
 
-    # Checks If A Matrix Row Is Not Occupied (All Items Equal To 'O' Or 'X')
+    # Checks If A Matrix Row Is Not Occupied (All Items Not Equal To 'O' Or 'X')
     # Example: [X, X, '-'], All Items Are Not Equal To Character '-', So The Row Is Available
     def available_game_board_rows
       game_matrix.map do |row|
@@ -24,28 +24,38 @@ Player One will be 'X' and Player Two will be 'O')
       end
     end
 
-    def select_option(player_symbol)
-      player_selection = Struct.new(:column, :row).new
+    def available_game_board_columns(selected_row)
+      # ['X','-','-'] -> [false, true, true]
+      selected_row.map { |item| item == '-' }
+    end
 
+    def player_prompt(player_selection, row_or_column, symbol, available_moves)
       options = %w[First Second Third].freeze
 
-      CLI::UI::Prompt.ask("#{player_symbol}: What row would you like to select?") do |handler|
-        available_game_board_rows.map.with_index do |move_available, index|
-          if move_available == true
-            handler.option(options[index]) do |selected_option|
-              player_selection.row = options.index(selected_option)
+      begin
+        CLI::UI::Prompt.ask("#{symbol}: What #{row_or_column} would you like to select?") do |handler|
+          available_moves.map.with_index do |move_available, index|
+            if move_available == true
+              handler.option(options[index]) do |selected_option|
+                player_selection[row_or_column] = options.index(selected_option)
+              end
             end
           end
         end
+      rescue Interrupt # https://github.com/Shopify/cli-ui/blob/master/lib/cli/ui/prompt/interactive_options.rb#L254-L257
+        puts 'Leaving Ruby Tic Tac Toe, Goodbye!'
+        exit
       end
+    end
 
-      CLI::UI::Prompt.ask("#{player_symbol}: What column would you like to select?") do |handler|
-        options.map do |option|
-          handler.option(option) do |selected_option|
-            player_selection.column = options.index(selected_option)
-          end
-        end
-      end
+    def select_option(player_symbol)
+      player_selection = Struct.new(:column, :row).new
+
+      player_prompt(player_selection, 'row', player_symbol, available_game_board_rows)
+
+      selected_row = game_matrix[player_selection.row]
+      available_columns = available_game_board_columns(selected_row)
+      player_prompt(player_selection, 'column', player_symbol, available_columns)
 
       player_selection
     end

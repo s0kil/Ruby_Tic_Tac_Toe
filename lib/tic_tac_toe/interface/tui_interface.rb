c7b8c2fbcc28f2ee9679b5f8f9f9f03ff9eadbe7
s0kil <dtechtours@gmail.com>
2020-08-01 00:22:24 -0400

    Small Tweaks

--- lib/tic_tac_toe/interface/tui_interface.rb
+++ lib/tic_tac_toe/interface/tui_interface.rb
@@ -31,13 +31,14 @@ module TicTacToe
         Curses.cbreak # Ctrl+C Exits The Program
 
         # The Number Is The Color Pair Identifier
-        @color_red_black = 1
-        # Setup Colors That Will Be Used In This Program
-        Curses.init_pair(@color_red_black, Curses::COLOR_RED, Curses::COLOR_BLACK)
+        @color_black_red = 1
+        # Setup Colors That Will Be Used Later
+        # Color Definitions: https://github.com/ruby/curses/blob/master/ext/curses/curses.c#L5216-L5263
+        Curses.init_pair(@color_black_red, Curses::COLOR_BLACK, Curses::COLOR_RED)
 
         @window = Curses::Window.new(0, 0, WINDOW_MARGIN, WINDOW_MARGIN) # Full Screen, With Some Margin
 
-        window.nodelay = true # Do Not Block Waiting For Keyboard Input With `getch`
+        window.nodelay = true # Do Not Block Waiting For Keyboard Input
         window.keypad = true # Allow User To Move Around With The Keyboard (Up, Down, Left, Right)
         window.refresh # Refreshes The Screen
 
@@ -54,11 +55,12 @@ Tic-tac-toe is a game for two players, X and O, who take turns marking the space
 The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row is the winner.
 )
 
-        window.addstr(Strings.wrap(welcome_message, 40))
+        window.addstr(Strings.wrap(welcome_message, 50))
 
         # Set Cursor To Bottom Of The Terminal, https://stackoverflow.com/a/54736503
-        window.setpos(window.maxy - WINDOW_MARGIN, 0)
-        window.addstr("Exit Game With Ctrl+C\n")
+        window.setpos(window.maxy - WINDOW_MARGIN - 2, 0)
+        window.addstr("- Press Space Bar To Mark An Empty Space\n")
+        window.addstr("- Exit Game With Ctrl+C\n")
         window.setpos(0, 0)
       end
 
@@ -66,7 +68,7 @@ The player who succeeds in placing three of their marks in a horizontal, vertica
         # TODO: How To Render In Free Space (After Welcome Message)? Versus Manually Hardcoding The Value
         window.setpos(10, 0)
 
-        window.attron(Curses.color_pair(@color_red_black)) do
+        window.attron(Curses.color_pair(@color_black_red)) do
           window.addstr("#{game_board[0][0]} | #{game_board[0][1]} | #{game_board[0][2]}\n")
           window.addstr("---------\n")
           window.addstr("#{game_board[1][0]} | #{game_board[1][1]} | #{game_board[1][2]}\n")
@@ -76,7 +78,7 @@ The player who succeeds in placing three of their marks in a horizontal, vertica
       end
 
       def start_game_loop
-        loop do # Render Loop
+        loop do
           yield # Assuming Block Givin
           window.refresh
         end
@@ -87,20 +89,19 @@ The player who succeeds in placing three of their marks in a horizontal, vertica
         # Close Screen Overlay After Game Loop Exits
         Curses.close_screen
 
-        # Goodbye Message
+        # Exit Message
         puts 'Thanks For Playing Tic Tac Toe, Goodbye!'
       end
 
       def handle_key_press
         # Make Sure Cursor Starts Inside The Game Board On First Render,
         # And Prevent Cursor From Escaping Game Board
-        cursor_coordinates[:y] = 10 if cursor_coordinates[:y] <= 10
-        cursor_coordinates[:y] = 14 if cursor_coordinates[:y] >= 14
+        cursor_coordinates[:y] = 10 if cursor_coordinates[:y] <= 10 # Up
+        cursor_coordinates[:y] = 14 if cursor_coordinates[:y] >= 14 # Down
+        cursor_coordinates[:x] = 0 if cursor_coordinates[:x] <= 0 # Left
+        cursor_coordinates[:x] = 8 if cursor_coordinates[:x] >= 8 # Right
 
-        cursor_coordinates[:x] = 0 if cursor_coordinates[:x] <= 0
-        cursor_coordinates[:x] = 8 if cursor_coordinates[:x] >= 8
-
-        # We Are Restoring Cursor Position Set By The User On The Previous Game Loop Cycle
+        # We Are Restoring Cursor Position Set On The Previous Game Loop Cycle
         window.setpos(
           cursor_coordinates[:y],
           cursor_coordinates[:x]
@@ -108,7 +109,7 @@ The player who succeeds in placing three of their marks in a horizontal, vertica
 
         key_press = window.getch
 
-        # Define The Amount Cursor Should Move On Each Key Press,
+        # Define Amount The Cursor Should Move On Each Key Press,
         # Example: One Game Board Row Is, `- | - | -`,
         # It Would Take Four Steps To Get To Each `-` Character In The Row
         row_cursor_gap = 2
